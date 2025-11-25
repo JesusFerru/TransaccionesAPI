198e12d4256936ac94c6d2086d9b3218326b8e44
Luis Ferrufino <lferrufino@estropical.com>
2025-11-24 21:08:14 -0400

    feat: add Handler exceptions with ProblemDetails format, automapper and serilog configuration

---
+++ Transacciones.API/Mapper/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Transacciones.API.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
--- Transacciones.API/Program.cs
+++ Transacciones.API/Program.cs
@@ -1,24 +1,74 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Transacciones.API.Mapper;
+using Transacciones.API.Middleware;
 using Transacciones.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddMemoryCache();
 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+builder.Services.AddProblemDetails(option =>
+{
+    option.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+        context.ProblemDetails.Extensions["traceId"] = context.HttpContext.TraceIdentifier;
+    };
+});
+builder.Services.AddExceptionHandler<HandleException>();
+
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("AllowAll", builder =>
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
+// OpenAPI Documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Transacciones API",
+        Version = "v1",
+        Description = "API para gestionar cuentas y transacciones bancarias."
+    });
+
+    options.EnableAnnotations();
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+app.UseExceptionHandler();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
